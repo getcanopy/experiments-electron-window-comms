d33b0f3c5b656c792b00dc2ea7053b876f824f7e
redaphid <iam@aaronherres.com>
2022-08-25 04:34:32 -0700

    try to add some comments to this hopeless mess

--- src/preload.ts
+++ src/preload.ts
@@ -9,7 +9,8 @@ const setupComms = () => {
   const id = Math.random() * 1000
   const portPromise = new Promise<MessagePort>((resolve, reject) => {
 
-    ipcRenderer.on('setup-comms', (event) => {
+    // listen to an ipc message that gives us a MessagePort to the server
+    ipcRenderer.once('setup-comms', (event) => {
       console.log(id, 'setup-comms')
       const server = event.ports[0]
       resolve(server)
@@ -17,6 +18,8 @@ const setupComms = () => {
   })
 
   const communicator = {
+
+    // This actually just asks the server to create a MessagePort to the browser view.
     message: async (msg) => {
       console.log(id, 'send message: wait for port', msg)
       const server = await portPromise
@@ -27,6 +30,9 @@ const setupComms = () => {
       console.log(id, 'receiving: wait for port')
       const server = await portPromise
       console.log(id, 'onMessage: got port')
+
+      // Listen to the server port for a message containing the MessagePort to the browser view.
+      // Yes, this adds a listener for every message. Which is bad.
       server.addEventListener('message', (event) => {
         console.log(id, 'onMessage: got message', event)
         if (event.ports.length > 0) {
