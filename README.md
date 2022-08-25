cbd1e06e58f7709e075f37c834066439262bc41e
redaphid <iam@aaronherres.com>
2022-08-24 22:37:02 -0700

    sending ports->server

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Just testing inter-window comms for electron. Other window-related stuff. Maybe something else? Who knows
 [Resize Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Resize_Observer_API)
 [Electron](https://electronjs.org/)
+[Potential Auth Access](https://developer.chrome.com/docs/extensions/reference/identity)
--- src/preload.ts
+++ src/preload.ts
@@ -1,8 +1,17 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-const {contextBridge} = require('electron')
-contextBridge.exposeInMainWorld('preloadTest', {
-  boring_function: () => {
-    return 'preloadTestTS'
-  }
-})
+const {contextBridge, ipcRenderer} = require('electron')
+const setupComms = () => {
+  console.log('setting up communications')
+  const {port1:server, port2:client} = new MessageChannel()
+  ipcRenderer.postMessage('setup-comms', null, [server])
+  contextBridge.exposeInMainWorld('comms', {
+    message: (msg) => {
+      client.postMessage('message', msg)
+    },
+    onMessage: (callback) => {
+      client.addEventListener('message', callback)
+    },
+  })
+}
+setupComms()
