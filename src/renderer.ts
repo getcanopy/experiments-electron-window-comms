c80f883be4beaca215ef7682891fbe8baab5f146
redaphid <iam@aaronherres.com>
2022-08-25 00:10:55 -0700

    add extension recommendation for preact templates

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,10 +1,16 @@
 import './index.css';
-import { h, Component, render } from 'preact';
+import { h, render } from 'preact';
 import htm from 'htm';
-
 const html = htm.bind(h);
+
 function App (props) {
-  return html`<h1>Hello ${props.name}!</h1>`;
+  return html`
+  <h1>This thing can probably control BrowserViews</h1>
+  <p>Now isn't that exciting.</p>
+  <div id="control-browser-views">
+    <button id="create-browser-view">Create BrowserView</button>
+    <button id="destroy-browser-view">Destroy BrowserViews</button>
+  </div>`
 }
 
-render(html`<${App} name="Everyone" />`, document.body);
+render(html`<${App}/>`, document.body);
